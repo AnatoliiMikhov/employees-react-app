0dbd09ca04dd935c0b8a0eb5af2ad351c1e281d1
Anatolii Mikhov <anatoli.mihov@gmail.com>
2022-08-22 02:51:50 +0300

    added comment to App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,48 @@
 import { useState } from 'react'
 
+/* ------------------------- uuid Generator for key ------------------------- */
 /* -- https://www.npmjs.com/package/uuid#uuidv5name-namespace-buffer-offset - */
 import { v4 as uuidv4 } from 'uuid'
 
+/* ---------------------------------- Data ---------------------------------- */
 import { employeesArray } from '../../data'
 
+/* ------------------------------- Components ------------------------------- */
 import AppInfo from '../AppInfo/AppInfo'
 import SearchPanel from '../SearchPanel/SearchPanel'
 import AppFilter from '../AppFilter/AppFilter'
 import EmployeesList from '../EmployeesList/EmployeesList'
 import EmployeesAddForm from '../EmployeesAddForm/EmployeesAddForm'
 
+/* --------------------------------- Styles --------------------------------- */
 import './App.css'
 
+/* ------------------------------ App Component ----------------------------- */
 function App () {
-
     const [ data, setData ] = useState( employeesArray )
     const [ term, setTerm ] = useState( '' )
 
     const visibleData = searchEmployees( data, term )
     const employees = data.length
-    const increased = data.filter( elem => elem.increase ).length
+    const increased = data.filter( ( elem ) => elem.increase ).length
+
+    return (
+        <div className='app'>
+            <AppInfo employees={ employees } increased={ increased } />
+
+            <div className='search-panel'>
+                <SearchPanel onUpdateSearch={ onUpdateSearch } />
+                <AppFilter />
+            </div>
+
+            <EmployeesList
+                data={ visibleData }
+                onDelete={ deleteItem }
+                onToggleProp={ onToggleProp }
+            />
+            <EmployeesAddForm onAddItem={ addItem } />
+        </div>
+    )
 
     function deleteItem ( id ) {
         setData( ( data ) => data.filter( ( item ) => item.id !== id ) )
@@ -40,16 +62,18 @@ function App () {
     }
 
     function onToggleProp ( id, prop ) {
-        setData( data => (
-            data.map( elem => ( elem.id === id ) ? { ...elem, [ prop ]: !elem[ prop ] } : elem )
-        ) )
+        setData( ( data ) =>
+            data.map( ( elem ) =>
+                elem.id === id ? { ...elem, [ prop ]: !elem[ prop ] } : elem
+            )
+        )
     }
 
     function searchEmployees ( items, term ) {
         if ( term.length === 0 ) {
             return items
         }
-        return items.filter( item => {
+        return items.filter( ( item ) => {
             return item.name.indexOf( term ) > -1
         } )
     }
@@ -57,26 +81,6 @@ function App () {
     function onUpdateSearch ( term ) {
         setTerm( term )
     }
-
-    return (
-        <div className='app'>
-            <AppInfo
-                employees={ employees }
-                increased={ increased } />
-
-            <div className='search-panel'>
-                <SearchPanel onUpdateSearch={ onUpdateSearch } />
-                <AppFilter />
-            </div>
-
-            <EmployeesList
-                data={ visibleData }
-                onDelete={ deleteItem }
-                onToggleProp={ onToggleProp }
-            />
-            <EmployeesAddForm onAddItem={ addItem } />
-        </div>
-    )
 }
 
 export default App
